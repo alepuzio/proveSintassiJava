0da9c826b9d7640ccb857363c917eb1e32a69cbb
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-01-19 11:38:56 +0100

    addnew classes

---
+++ src/net/alepuzio/provesintassi/lambda/TypeInferenceLambdaExpressionSevenMinutesLambda.java
@@ -0,0 +1,10 @@
+package net.alepuzio.provesintassi.lambda;
+
+import java.util.*;
+
+public class TypeInferenceLambdaExpressionSevenMinutesLambda {
+	public static void main(String[] args) {
+		List<String> strings = Arrays.asList("Lambda ", "expressions ", "are ", "cool");
+		strings.forEach(s -> System.out.print(s));
+	}
+}
