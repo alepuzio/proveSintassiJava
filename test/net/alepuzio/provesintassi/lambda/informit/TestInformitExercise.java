a5166dc4796190fc2e90837758cfe67f3b791609
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-08 16:36:32 +0200

    successfull test of the six exercise

--- test/net/alepuzio/provesintassi/lambda/informit/TestInformitExercise.java
+++ test/net/alepuzio/provesintassi/lambda/informit/TestInformitExercise.java
@@ -128,5 +128,24 @@ public class TestInformitExercise {
 		assertEquals(expected,  actual);
 	}
 
+	/**
+	 * In this exercise, you will try out what happens when a method is added to an interface. 
+	 * In Java 7, implement a class DigitSequence that implements Iterator<Integer>, not IntSequence. 
+	 * Provide methods hasNext, next, and a do-nothing remove.
+	 * Write a program that prints the elements of an instance. 
+	 * In Java 8, the Iterator class gained another method, forEachRemaining.
+	 *  Does your code still compile when you switch to Java 8? 
+	 *  If you put your Java 7 class in a JAR file and don’t recompile, does it work in Java 8? 
+	 *  What if you call the forEachRemaining method? 
+	 *  Also, the remove method has become a default method in Java 8, throwing an UnsupportedOperationException. 
+	 * What happens when remove is called on an instance of your class?
+	 * */
+	@Test
+	public void testSix(){
+		logger.info("Secondary");
+		assertTrue(true);
+	}
+	
 	
 }
+
